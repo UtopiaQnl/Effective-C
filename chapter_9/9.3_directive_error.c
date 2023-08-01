1b2438d8889916ac18c8b8d18d2a90c03be495d8
Ivan Kovach <eeiprex@gmail.com>
2023-08-01 12:36:14 +0300

    [chapter 9]: Add listing `9.3`
    Add expamle using preprocessing directive '#error'.

---
+++ chapter_9/9.3_directive_error.c
@@ -0,0 +1,7 @@
+#if _STDC__ && __STDC_NO_THREADS__ != 1
+#include <threads.h>
+#elif POSIX_THREADS == 200809L
+#include <pthread.h>
+#else
+#error Neither <threads.h> nor <pthread.h> is available
+#endif
